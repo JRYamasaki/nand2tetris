a7e01d1a70e341b793ed87951631e7def7508265
JRYamasaki <yamasakijeffrey@gmail.com>
2024-03-31 16:34:16 -0700

    Create Multiplcation function in Hack

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,47 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// You can add or subtract in hack
+// Need a for loop, takes two numbers in the ram, use a for loop to multiply them together and store them in R2
+
+// How do we add numbers?
+// How do we store the result in the R2?
+// How do we do this for two numbers?
+    // How can we expand this function to do it for all numbers in the register?
+
+// Set inputs for test
+@0
+D=A
+@2
+M=D
+
+(LOOP)
+    // jump to end if @1 or @0 is zero
+    @1
+    D=M
+    @END
+    D;JEQ
+
+    @0
+    D=M
+    @END
+    D;JEQ
+
+    // load @0
+    @0
+    D=M
+
+    // add @0 to sum
+    @2
+    M=D+M
+
+    // subtract 1 from @1
+    @1
+    M=M-1
+
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
