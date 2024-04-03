4bda53dd048350587256cbf325986fa129d8b6f8
JRYamasaki <yamasakijeffrey@gmail.com>
2024-04-02 21:56:37 -0700

    Fill screen with blakc pixels

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,48 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// keyboard input kept in 24576, @KBD
+
+@SCREEN
+D=A
+@addr
+M=D
+
+@8192 // the number of 16 width pixels we need to set, 256 * (512 / 16)
+D=A
+@0
+M=D
+
+@0 // the number of times we should iterate
+D=M
+@n
+M=D
+
+@i
+M=0
+
+(LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JGT // check if i < n
+
+    @addr
+    A=M
+    M=-1 // set row to be black
+
+    @i
+    M=M+1 // increment i
+    @1
+    D=A
+    @addr
+    M=D+M // move to the next 16 set of pixels
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
